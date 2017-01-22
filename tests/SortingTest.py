63f59c248c6d12172b9141dceb21c39f1baf832b
Mishk0 <mshelemetev@gmail.com>
2017-01-22 18:37:58 -0500

    need 11 members of array to properly test merging sorting

--- tests/SortingTest.py
+++ tests/SortingTest.py
@@ -7,7 +7,7 @@ from algos.sorting import SelectionSorting
 
 class TestStringMethods(unittest.TestCase):
 
-    testArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    testArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
 
     originalArray = copy.copy(testArray)
 
